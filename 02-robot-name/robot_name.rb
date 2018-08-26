b365ce883826fa0d591be6345d5555e803c42a6a
Amanda Ungco <amanda.ungco@gmail.com>
2018-08-26 15:55:23 -0700

    added robot-name generator and reset

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,21 @@
+class Robot
+  def initialize
+    name_array = ("A".."Z").to_a.sample(2)
+    three_numbers = rand(111..999).to_s
+    name_array << three_numbers
+    @name = name_array.join
+    return @name
+  end
 
+  attr_reader :name
+
+
+  def reset
+    name_array = ("A".."Z").to_a.sample(2)
+    three_numbers = rand(111..999).to_s
+    name_array << three_numbers
+    @name = name_array.join
+    return @name
+  end
+
+end
