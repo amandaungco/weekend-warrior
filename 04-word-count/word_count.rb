d87d1e6c593f0233a739dfa0db5d8cc00722f359
Amanda Ungco <amanda.ungco@gmail.com>
2018-09-15 09:15:30 -0700

    all tests pass

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -2,7 +2,10 @@ def words(string)
   words = string.split(' ')
   counts = {}
   words.each do |word|
-    counts[word] = 1
+    if counts.include? word
+      counts[word] += 1
+    else counts[word] = 1
+    end
   end
   return counts
 end
